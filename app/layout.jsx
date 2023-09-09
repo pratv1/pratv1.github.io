f74cfefbaef3f62eb81d730f0d9a5251b1673b5c
Pratik <npc.prat@gmail.com>
2023-09-10 00:40:11 +0530

    Make it dark

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,45 +19,47 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${firaCode.variable} font-sans`}>
+      <body
+        className={`${inter.variable} ${firaCode.variable} bg-zinc-900 font-sans text-white`}
+      >
         <main className="relative overflow-hidden">
-          <div className="pointer-events-none absolute left-1/2 top-0 z-10 flex h-20 w-[1280px] -translate-x-1/2 justify-center text-slate-900">
-            <div className="absolute h-full w-[1280px] border-x border-dashed border-slate-300">
-              <span className="p-1 font-mono text-xs opacity-50">1280px</span>
-              <span className="absolute right-0 p-1 font-mono text-xs opacity-50">
+          <div className="pointer-events-none absolute left-1/2 top-0 z-10 hidden h-20 w-[1280px] -translate-x-1/2 justify-center text-white opacity-20 sm:flex">
+            <div className="absolute h-full w-[1280px] border-x border-dashed border-white">
+              <span className="p-1 font-mono text-xs">1280px</span>
+              <span className="absolute right-0 p-1 font-mono text-xs">
                 1280px
               </span>
             </div>
-            <div className="absolute h-full w-[1024px] border-x border-dashed border-slate-300">
-              <span className="p-1 font-mono text-xs opacity-50">1024px</span>
-              <span className="absolute right-0 p-1 font-mono text-xs opacity-50">
+            <div className="absolute h-full w-[1024px] border-x border-dashed border-white">
+              <span className="p-1 font-mono text-xs">1024px</span>
+              <span className="absolute right-0 p-1 font-mono text-xs">
                 1024px
               </span>
             </div>
-            <div className="absolute h-full w-[768px] border-x border-dashed border-slate-300">
-              <span className="p-1 font-mono text-xs opacity-50">768px</span>
-              <span className="absolute right-0 p-1 font-mono text-xs opacity-50">
+            <div className="absolute h-full w-[768px] border-x border-dashed border-white">
+              <span className="p-1 font-mono text-xs">768px</span>
+              <span className="absolute right-0 p-1 font-mono text-xs">
                 768px
               </span>
             </div>
-            <div className="absolute h-full w-[640px] border-x border-dashed border-slate-300">
-              <span className="p-1 font-mono text-xs opacity-50">640px</span>
-              <span className="absolute right-0 p-1 font-mono text-xs opacity-50">
+            <div className="absolute h-full w-[640px] border-x border-dashed border-white">
+              <span className="p-1 font-mono text-xs">640px</span>
+              <span className="absolute right-0 p-1 font-mono text-xs">
                 640px
               </span>
             </div>
-            <div className="absolute h-full w-[360px] border-x border-dashed border-slate-300">
-              <span className="p-1 font-mono text-xs opacity-50">360px</span>
-              <span className="absolute right-0 p-1 font-mono text-xs opacity-50">
+            <div className="absolute h-full w-[360px] border-x border-dashed border-white lg:hidden">
+              <span className="p-1 font-mono text-xs">360px</span>
+              <span className="absolute right-0 p-1 font-mono text-xs">
                 360px
               </span>
             </div>
-            <div className="absolute h-full w-full bg-gradient-to-b from-transparent from-50% to-slate-50" />
+            <div className="absolute h-full w-full bg-gradient-to-b from-transparent from-50% to-zinc-950" />
           </div>
           {children}
         </main>
-        <footer className="bg-gradient-to-r from-indigo-600 via-sky-600 to-emerald-600 shadow-inner">
-          <div className="container py-5 text-center font-medium text-white">
+        <footer className="bg-zinc-950 shadow-inner">
+          <div className="container py-6 text-center font-medium text-slate-300">
             Designed and coded by... <i>*drumroll*</i> ... me!
           </div>
         </footer>
--- app/page.jsx
+++ app/page.jsx
@@ -8,32 +8,60 @@
 import {
   KeyboardDoubleArrowDown,
   Laptop,
-  LinkedIn,
   SchoolOutlined,
   WorkOutline,
 } from "@mui/icons-material";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <>
-      <section className="relative border-b border-slate-200 bg-gradient-to-r from-indigo-100 via-sky-100 to-emerald-100">
-        <div className="absolute bottom-0 left-0 h-full w-full bg-gradient-to-t from-transparent to-slate-50 to-15%"></div>
+      <section className="relative bg-zinc-950">
+        <motion.div
+          className="absolute bottom-0 left-0 h-full w-full bg-gradient-to-r from-gray-950 via-gray-900 to-gray-950"
+          animate={{
+            x: ["0%", "100%"],
+            opacity: [1, 0.5, 1],
+            transition: {
+              duration: 5,
+              ease: "linear",
+              repeat: Infinity,
+            },
+          }}
+        />
+        <motion.div
+          className="absolute bottom-0 left-0 h-full w-full bg-gradient-to-r from-gray-950 via-gray-900 to-gray-950"
+          animate={{
+            scaleX: 1.001, // Prevents gap between the two gradients
+            x: ["-100%", "0%"],
+            opacity: [1, 0.5, 1],
+            transition: {
+              duration: 5,
+              ease: "linear",
+              repeat: Infinity,
+            },
+          }}
+        />
+        <div className="absolute bottom-0 left-0 h-full w-full bg-gradient-to-t from-transparent to-zinc-950 to-10%" />
         <div className="container relative max-w-screen-lg py-28 text-center md:py-40">
-          <h1 className="mt-10 text-4xl font-extrabold text-slate-900 sm:text-5xl md:text-6xl">
-            Hello, I'm Pratik Var.
+          <h1 className="text-4xl font-extrabold sm:mt-10 sm:text-5xl md:text-6xl">
+            Hello, I'm{" "}
+            <span className="bg-gradient-to-r from-blue-200 to-blue-400 bg-clip-text text-transparent">
+              Pratik Var.
+            </span>
           </h1>
-          <div className="mt-8 leading-relaxed text-slate-600 md:text-lg">
+          <div className="mt-8 leading-relaxed text-slate-300 md:text-lg">
             <p className="mb-3">
-              I'm a 21-year-old self-taught{" "}
-              <b className="font-medium text-slate-900">front-end developer</b>{" "}
-              based in <b className="font-medium text-slate-900">India</b>, with
-              a passion for creating beautiful, intuitive user experiences. I'm
+              I'm a self-taught{" "}
+              <b className="font-medium text-white">front-end developer</b>{" "}
+              based in <b className="font-medium text-white">India</b>, with a
+              passion for creating beautiful, intuitive user experiences. I'm
               currently working at{" "}
               <a
                 href="https://bolt.earth/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="font-medium text-slate-900 underline decoration-slate-400 decoration-dotted underline-offset-2 hover:text-blue-600"
+                className="font-medium text-white underline decoration-slate-500 decoration-dotted underline-offset-4 hover:text-blue-300"
               >
                 Bolt.Earth
               </a>
@@ -41,16 +69,14 @@ const Home = () => {
             </p>
             <p>
               My current go-to stack is{" "}
-              <span className="font-medium text-slate-900">
-                React (Next.js)
-              </span>
-              , <span className="font-medium text-slate-900">MUI</span>, and{" "}
-              <span className="font-medium text-slate-900">Tailwind CSS</span>.
+              <span className="font-medium text-white">React (Next.js)</span>,{" "}
+              <span className="font-medium text-white">MUI</span>, and{" "}
+              <span className="font-medium text-white">Tailwind CSS</span>.
             </p>
           </div>
         </div>
         <button
-          className="absolute bottom-0 left-1/2 z-20 flex -translate-x-1/2 translate-y-1/2 cursor-pointer items-center rounded-full bg-slate-700 px-4 py-2 text-sm font-semibold text-white shadow-xl transition-all hover:bg-slate-600 active:bg-slate-800"
+          className="absolute bottom-0 left-1/2 z-20 flex -translate-x-1/2 translate-y-1/2 cursor-pointer items-center rounded-full border-2 border-white bg-white px-4 py-2 text-sm font-semibold text-black shadow-xl transition-all hover:bg-zinc-950 hover:text-white"
           onClick={() => {
             const el = document.getElementById(
               "background-and-work-experience",
@@ -66,19 +92,25 @@ const Home = () => {
         </button>
       </section>
 
-      <section id="background-and-work-experience" className="pt-4">
+      <section
+        id="background-and-work-experience"
+        className="border-t border-zinc-800 pt-4"
+      >
         <div className="container py-20">
-          <h3 className="mb-8 text-3xl font-extrabold text-slate-900 sm:text-4xl">
-            Background and Work Experience
+          <div className="mb-4 text-xs font-semibold uppercase tracking-wider text-blue-300">
+            Background
+          </div>
+          <h3 className="mb-10 text-3xl font-extrabold sm:text-4xl">
+            Education and Work Experience
           </h3>
           <div className="flex items-stretch space-x-4">
             <div className="flex w-7 flex-col items-center">
-              <div className="relative my-3 text-purple-500">
+              <div className="relative my-3 text-purple-300">
                 <div
                   className="absolute left-1/2 top-1/2 z-0 h-14 w-14 -translate-x-1/2 -translate-y-1/2"
                   style={{
                     background:
-                      "radial-gradient(rgb(250, 245, 255) 15%, transparent 75%)",
+                      "radial-gradient(rgb(88, 28, 135), transparent 75%)",
                   }}
                 />
                 <SchoolOutlined
@@ -87,20 +119,20 @@ const Home = () => {
                   className="relative"
                 />
               </div>
-              <div className="h-full w-[2px] rounded bg-gradient-to-b from-purple-500 from-50% to-indigo-500" />
+              <div className="h-full w-[2px] rounded bg-gradient-to-b from-purple-400 from-70% to-indigo-400" />
             </div>
-            <div className="pb-12">
-              <div className="mb-2 text-sm font-semibold text-purple-800">
+            <div className="max-w-2xl pb-14">
+              <div className="mb-2 text-sm font-semibold text-purple-300">
                 Apr 2015 – Apr 2019
               </div>
-              <div className="mb-4 text-lg text-slate-900">
+              <div className="mb-4 text-lg">
                 <b className="font-bold">Alwar Public School, Rajasthan</b>{" "}
-                <span className="text-slate-600">
+                <span className="text-gray-400">
                   {" "}
                   — Secondary School (CBSE 10+2)
                 </span>
               </div>
-              <p className="text-slate-500">
+              <p className="text-gray-300">
                 I was first introduced to HTML in 2016, when I was in 10th
                 grade. I found it very fun to be able to write some code and see
                 it render on the screen. I started learning more about HTML and
@@ -110,12 +142,12 @@ const Home = () => {
           </div>
           <div className="flex items-stretch space-x-4">
             <div className="flex w-7 flex-col items-center">
-              <div className="relative my-3 text-indigo-500">
+              <div className="relative my-3 text-indigo-300">
                 <div
                   className="absolute left-1/2 top-1/2 z-0 h-14 w-14 -translate-x-1/2 -translate-y-1/2"
                   style={{
                     background:
-                      "radial-gradient(rgb(238, 242, 255) 15%, transparent 75%)",
+                      "radial-gradient(rgb(49, 46, 129), transparent 75%)",
                   }}
                 />
                 <Laptop
@@ -124,13 +156,13 @@ const Home = () => {
                   className="relative"
                 />
               </div>
-              <div className="h-full w-[2px] rounded bg-gradient-to-b from-indigo-500 from-50% to-emerald-500 to-90%" />
+              <div className="h-full w-[2px] rounded bg-gradient-to-b from-indigo-400 from-70% to-emerald-400" />
             </div>
-            <div className="pb-12">
-              <div className="mb-2 text-sm font-semibold text-indigo-800">
+            <div className="max-w-2xl pb-14">
+              <div className="mb-2 text-sm font-semibold text-indigo-300">
                 May 2019 – Oct 2020
               </div>
-              <div className="mb-4 text-lg text-slate-900">
+              <div className="mb-4 text-lg">
                 <a
                   href="https://freecodecamp.org"
                   target="_blank"
@@ -139,16 +171,16 @@ const Home = () => {
                 >
                   freeCodeCamp.org
                 </a>{" "}
-                <span className="text-slate-600">
+                <span className="text-gray-400">
                   {" "}
                   — Web Development Courses
                 </span>
               </div>
-              <div className="text-slate-500">
-                <p className="mb-2">
-                  <b className="font-medium text-slate-900">freeCodeCamp</b> is
-                  an online learning platform that puts an emphasis on learning
-                  by doing. I completed the following certifications:
+              <div className="text-gray-300">
+                <p className="mb-4">
+                  <b className="font-medium">freeCodeCamp</b> is an online
+                  learning platform that puts an emphasis on learning by doing.
+                  I completed the following certifications:
                 </p>
                 <div className="mb-2 flex flex-wrap items-center">
                   {[
@@ -175,7 +207,7 @@ const Home = () => {
                   ].map((el, i) => (
                     <a
                       key={i}
-                      className="mb-2 mr-2 rounded-full border px-2 py-1 text-sm font-medium text-slate-900 transition-all hover:border-indigo-200 hover:bg-indigo-50"
+                      className="mb-3 mr-3 rounded-full border border-zinc-700 bg-zinc-800 px-2 py-1 text-sm font-medium text-white transition-all  hover:bg-zinc-700"
                       href={el.link}
                       target="_blank"
                       rel="noopener noreferrer"
@@ -201,12 +233,12 @@ const Home = () => {
           </div>
           <div className="flex items-stretch space-x-4">
             <div className="flex w-7 flex-col items-center">
-              <div className="relative my-3 text-emerald-500">
+              <div className="relative my-3 text-emerald-300">
                 <div
-                  className="absolute left-1/2 top-1/2 z-0 h-14 w-14 -translate-x-1/2 -translate-y-1/2"
+                  className="emeral absolute left-1/2 top-1/2 z-0 h-14 w-14 -translate-x-1/2 -translate-y-1/2"
                   style={{
                     background:
-                      "radial-gradient(rgb(236, 253, 245) 15%, transparent 75%)",
+                      "radial-gradient(rgb(6, 78, 59), transparent 75%)",
                   }}
                 />
                 <WorkOutline
@@ -215,17 +247,17 @@ const Home = () => {
                   sx={{ fontSize: 28 }}
                 />
               </div>
-              <div className="h-full w-[2px] rounded-full bg-gradient-to-b from-emerald-500 from-20% to-transparent to-90%" />
+              <div className="h-full w-[2px] rounded-full bg-gradient-to-b from-emerald-400 from-20% to-transparent to-90%" />
             </div>
-            <div>
-              <div className="mb-2 text-sm font-semibold text-emerald-800">
+            <div className="max-w-2xl">
+              <div className="mb-2 text-sm font-semibold text-emerald-300">
                 Nov 2020 – Present
               </div>
-              <div className="mb-4 text-lg text-slate-900">
+              <div className="mb-4 text-lg">
                 <b className="font-bold">Bolt.Earth, Bengaluru (Remote)</b>{" "}
-                <span className="text-slate-600"> — Front-End Developer</span>
+                <span className="text-gray-400"> — Front-End Developer</span>
               </div>
-              <div className="text-slate-500">
+              <div className="text-gray-300">
                 <p className="leading-relaxed">
                   I started out as an intern at Bolt.Earth, and was later hired
                   as a full-time employee. My main responsibilities include:
@@ -268,9 +300,27 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="border-t border-slate-200 bg-slate-50">
+
+      <section className="border-t border-zinc-800">
         <div className="container py-20">
-          <h3 className="mb-8 text-3xl font-extrabold text-slate-900 sm:text-4xl">
+          <div className="mb-4 text-xs font-semibold uppercase tracking-wider text-amber-300">
+            Skills
+          </div>
+          <h3 className="mb-10 text-3xl font-extrabold sm:text-4xl">
+            Technologies I've Worked With
+          </h3>
+          <div>
+            <span className="font-mono">{"< In Progress >"}</span>
+          </div>
+        </div>
+      </section>
+
+      <section className="border-t border-zinc-800">
+        <div className="container py-20">
+          <div className="mb-4 text-xs font-semibold uppercase tracking-wider text-emerald-300">
+            Projects
+          </div>
+          <h3 className="mb-8 text-3xl font-extrabold sm:text-4xl">
             Some of My Work
           </h3>
           <div>
@@ -278,10 +328,14 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="border-t border-slate-200">
+
+      <section className="border-t border-zinc-800">
         <div className="container py-20">
-          <h3 className="mb-8 text-3xl font-extrabold text-slate-900 sm:text-4xl">
+          <div className="mb-4 text-xs font-semibold uppercase tracking-wider text-rose-300">
             Contact
+          </div>
+          <h3 className="mb-10 text-3xl font-extrabold sm:text-4xl">
+            Get In Touch
           </h3>
           <div className="flex items-center space-x-2">
             {[
@@ -304,12 +358,12 @@ const Home = () => {
             ].map((el, i) => (
               <a
                 key={i}
-                className="flex items-center rounded-full border border-slate-300 px-3 py-1 transition-all hover:border-slate-400"
+                className="flex items-center rounded-full border border-zinc-700 px-3 py-1 transition-all"
                 href={el.link}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <span className="font-medium text-slate-900">{el.name}</span>
+                <span className="font-medium ">{el.name}</span>
               </a>
             ))}
           </div>
